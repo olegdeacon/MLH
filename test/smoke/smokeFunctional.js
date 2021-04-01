af1fad54bc3a4f043f3f909d7a6730da16d35bb4
olegdeacon <o_deacon@hotmail.com>
2021-03-31 19:24:52 -0600

    Mar31

--- test/smoke/smokeFunctional.js
+++ test/smoke/smokeFunctional.js
@@ -1,4 +1,6 @@
 import sel from '../../data/selectors';
+import {name, gender, age, story} from '../../data/testData';
+import inputValues4 from '../../helpers/methods';
 
 describe('Required fields and story created', function () {
 
@@ -7,26 +9,19 @@ describe('Required fields and story created', function () {
     });
 
     it('TC-026 Submit button is enable after fields 1-4 are filled in with valid values', function () {
-        $(sel.name).setValue('LadyBug007');
-        $$(sel.radioButtons)[1].click();
-        $(sel.age).setValue('1234567890');
+        $(sel.name).setValue(name.default);
+        $$(sel.radioButtons)[gender.she].click();
+        $(sel.age).setValue(age.default);
         $(sel.storyType).click();
-        $$(sel.storyList)[6].click();
+        $$(sel.storyList)[story.comedy].click();
         let submitBtn = $(sel.submit).isEnabled();
         expect(submitBtn).toEqual(true);
     })
 
-    it('TC-027 Submit button is enable after fields 1-4 are filled in with valid values', function () {
+    it.only('TC-027 Submit button is enable after fields 1-4 are filled in with valid values', function () {
         browser.refresh();
-
-        $(sel.name).setValue('LadyBug007');
-        $$(sel.radioButtons)[1].click();
-        $(sel.age).setValue('1234567890');
-        $(sel.storyType).click();
-        $$(sel.storyList)[6].click();
+        inputValues4(name.default, gender.she, age.default, story.comedy);
         $(sel.submit).click();
-
-        browser.pause(2000);
         let tryAgainButton = $(sel.tryAgain).isDisplayed();
         expect(tryAgainButton).toEqual(true);
     });
--- wdio.conf.js
+++ wdio.conf.js
@@ -17,14 +17,12 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/extended/*.js'
+        './test/smoke/smokeFunctional.js'
     ],
     // Patterns to exclude.
     exclude: [
         './test/smoke/elementsExist.js',
         './test/smoke/labelsCorrect.js',
-        './test/smoke/*.js',
-
     ],
     //
     // ============
@@ -93,7 +91,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'https://qa-apps.netlify.app/hero ',
+    baseUrl: 'https://qa-apps.netlify.app/hero/fix',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
